a727ce8da74b493b5d5340614860b2349a10e4fd
Sven van Bezooijen <svenvanbezooijen@hotmail.com>
2021-12-11 22:13:29 +0100

    Update Version 0.2

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+function If_statement(type){
+    var $response;
+    if()
+    {
+        document.getElementById("True",id).innerHTML;
+    }
+
+    else
+    {
+        response = "Er is geen couveuse actief op dit tijdstip";
+    }
+document.getElementById("Not_true").innerHTML = response;
+}
